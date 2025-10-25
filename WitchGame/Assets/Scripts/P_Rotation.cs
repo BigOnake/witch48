49d734e182676808a2cf20af44ce5263e2b93bf4
Urmat Urustemov <urustemovurmatk@gmail.com>
2025-10-25 02:29:26 -0500

    Controller rotation

---
+++ WitchGame/Assets/Scripts/P_Rotation.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class P_Rotation : MonoBehaviour
+{
+    #region Fields
+    private Vector3 lookingInputs;
+    public float sensitivity = 0.1f;
+    #endregion
+
+    #region GameEngineLoop
+    private void OnEnable()
+    {
+        //Debug.Log("<color=green> Camera script is enabled. </color>");
+        InputController.onPlayerLook += ReadInputs;
+    }
+
+    private void OnDisable()
+    {
+        //Debug.Log("<color=red> Camera script is disabled. </color>");
+        InputController.onPlayerLook -= ReadInputs;
+    }
+
+    void Update()
+    {
+        Look();
+    }
+    #endregion
+
+    #region Inputs
+    public void ReadInputs(Vector2 input)
+    {
+        lookingInputs = new Vector3(input.x, 0, input.y);
+    }
+    #endregion
+
+    #region Movement
+    public void Look()
+    {
+        if (lookingInputs != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(-lookingInputs); //works great w/ gamepad
+        }
+    }
+    #endregion
+}
